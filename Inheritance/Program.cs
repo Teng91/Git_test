73391b267eb431530df69395cccdc8a10dbfe1ff
Teng91 <joy68304276@gmail.com>
2024-12-09 10:54:14 +0800

    some commit message

---
+++ Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Joy");
+            Console.WriteLine(car.Owner);
+        }
+    }
+
+    class Vehicle // 基類(父類)Base
+    {
+        public Vehicle(string owner)
+        {
+            this.Owner = owner;
+        }
+        public string Owner { get; set; }
+    }
+
+    class Car : Vehicle // 派生類(子類)Derive
+    {
+        // 父類的實例構造器不會被子類繼承
+        public Car(string owner): base(owner)
+        {
+            this.Owner = owner;
+        }
+
+        public void ShowOwner()
+        {
+            Console.WriteLine(Owner);
+        }
+    }
+}
