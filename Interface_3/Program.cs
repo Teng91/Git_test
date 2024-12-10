175dfe06da1e70c0a31690aeb5b811dd03c7fe1e
Teng91 <joy68304276@gmail.com>
2024-12-10 17:17:23 +0800

    new files upload

---
+++ Interface_3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace IspExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new Driver(new Car());
+            driver.Drive();
+        }
+    }
+
+    class Driver
+    {
+        private IVehicle _vehicle;
+        public Driver(IVehicle vehicle)
+        {
+            _vehicle = vehicle;
+        }
+
+        public void Drive()
+        {
+            _vehicle.Run();
+        }
+    }
+
+    interface IVehicle
+    {
+        void Run();
+    }
+
+    class Car : IVehicle
+    {
+        public void Run()
+        {
+            Console.WriteLine("Car is running...");
+        }
+    }
+
+    class Truck : IVehicle
+    {
+        public void Run()
+        {
+            Console.WriteLine("Truck is running...");
+        }
+    }
+
+    // 解法: 將一個接口拆成兩個(另一個是原本的IVehicle)
+    interface IWeapon
+    {
+        void Fire();
+    }
+
+    interface ITank : IVehicle, IWeapon // 分離接口
+    {
+    }
+
+    // interface ITank
+    // {
+    //     void Fire(); // 永遠有功能用不到 -> 違反接口隔離原則
+    //     void Run();
+    // }
+
+    class LightTank : ITank
+    {
+        public void Fire()
+        {
+            System.Console.WriteLine("Boom!");
+        }
+
+        public void Run()
+        {
+            System.Console.WriteLine("Ka Ka Ka...");
+        }
+    }
+
+    class MediumTank : ITank
+    {
+        public void Fire()
+        {
+            System.Console.WriteLine("Boom!!");
+        }
+
+        public void Run()
+        {
+            System.Console.WriteLine("Ka! Ka! Ka!...");
+        }
+    }
+
+    class HeavyTank : ITank
+    {
+        public void Fire()
+        {
+            System.Console.WriteLine("Boom!!!");
+        }
+
+        public void Run()
+        {
+            System.Console.WriteLine("Ka!! Ka!! Ka!!...");
+        }
+    }
+}
---
+++ Interface_4/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+
+namespace IspExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 3, 4, 5 };
+            ArrayList nums2 = new ArrayList { 1, 2, 3, 4, 5 };
+            System.Console.WriteLine(Sum(nums1));
+            System.Console.WriteLine(Sum(nums2));
+            var nums3 = new ReadOnlyCollection(nums1);
+            System.Console.WriteLine(Sum(nums3));
+        }
+
+        // static int Sum(ICollection nums) // ICollection為系統內建的基底介面
+        static int Sum(IEnumerable nums) // 傳進來的接口不應該有使用不到的功能
+        {
+            int sum = 0;
+            foreach (var n in nums)
+            {
+                sum += (int)n; // ArrayList需要強制類型轉換
+            }
+
+            return sum;
+        }
+
+    class ReadOnlyCollection : IEnumerable
+    {
+        private int[] _array;
+
+        public ReadOnlyCollection(int[] array)
+        {
+            _array = array;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public class Enumerator : IEnumerator
+        {
+            private ReadOnlyCollection _collection;
+            private int _head;
+
+            public Enumerator(ReadOnlyCollection collection)
+            {
+                _collection = collection;
+                _head = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    object o = _collection._array[_head];
+                    return o; // 這邊不能直接返回整數 -> 需裝箱
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (++_head < _collection._array.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset()
+            {
+                _head = -1;
+            }
+        }
+    }
+
+    }   
+}
